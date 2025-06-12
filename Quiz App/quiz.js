9bf7e55a682f97c34d14539c88bc17c55232ef55
nandani <nandanigupta9505@gmail.com>
2025-06-12 23:27:52 +0530

    neww

--- Quiz App/quiz.js
+++ Quiz App/quiz.js
@@ -4,3 +4,11 @@ function submitQuiz(event) {
   // Get form data and calculate score here
   console.log("Quiz submitted!");
 }
+// navbar page;
+let menu =document.querySelector('#menu-icon');
+let navbar =document.querySelector('.navbar');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('open');
+}
