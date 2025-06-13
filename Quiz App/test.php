142aa4f7ca56fc8f9c070a57049633236e06e872
tulsi <ttulsigupta836@gmail.com>
2025-06-14 03:04:36 +0530

    chan

--- Quiz App/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "quiz_app");
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "✅ Connected successfully to quiz_app!";
-?>
