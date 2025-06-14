e43ed3d01b0d750fcd66b2393485e4b28cc9f9f8
tulsi <ttulsigupta836@gmail.com>
2025-06-15 00:35:19 +0530

    new

--- Quiz App/score.php
+++ Quiz App/score.php
@@ -9,14 +9,60 @@ $quizName = "General Quiz";
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
+
 <head>
     <meta charset="UTF-8">
-    <title>Scoreboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiz App</title>
     <link rel="stylesheet" href="quiz.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+        rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+           <!-- navbar page -->
+     <link
+    href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css"
+    rel="stylesheet"
+/>
+  <link rel="stylesheet"
+  href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600&display=swap" rel="stylesheet">
+
     
 </head>
 <body>
+   <!-- navbar page -->
+     <div class ="navbar">
+        <header>
+        <a href="#" class="logo"><i class="ri-home-heart-fill"></i><span>Quiz App</span></a>
+        <ul class ="navbar">
+            <li><a href="Home.html" class ="active">Home</a></li>
+            <li><a href="About.html">About us</a></li>
+            <li><a href="Scoreboard.html">Scoreboard</a></li>
+            <li><a href="new.php">Quiz</a></li>
 
+            
+                <li><a href="Contact.html">Contact Us</a></li>
+            <li><a href="Login.html">Log in</a></li>
+            
+
+
+        </ul>
+
+        <div class="main">
+            <a href="#" class=""user><i class="ri-user-fill"></i>sign In</a>
+            <div class="bx bx-menu" id="menu icon"></div>
+        </div>
+        </header>
+
+
+     </div>
 <div class="score-outer">
     <div class="score-user">
         <h2 class="score-h2">Welcome </h2>
